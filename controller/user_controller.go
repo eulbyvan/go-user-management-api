0c69f2ff9de615671c771f97d50158767420348a
eulbyvan <work.issdq@gmail.com>
2023-03-05 17:34:52 +0700

    Fix add user

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -17,54 +17,54 @@ import (
 	"github.com/gin-gonic/gin"
 )
  
- type UserController struct {
-	 BaseController
-	 router  *gin.RouterGroup
-	 usecase usecase.UserUsecase
- }
- 
- func NewUserController(r *gin.RouterGroup, u usecase.UserUsecase) *UserController {
-	 controller := UserController{
-		 router:  r,
-		 usecase: u,
-	 }
- 
-	 // define routes
-	 r.GET("/users", controller.GetAll)
-	 r.GET("/users/:id", controller.GetOne)
-	 r.POST("/users", controller.Add)
-	 r.PUT("/users/:id", controller.Edit)
-	 r.DELETE("/users/:id", controller.Remove)
- 
-	 return &controller
- }
- 
- func (c *UserController) GetAll(ctx *gin.Context) {
-	 res, err := c.usecase.GetAll()
-	 if err != nil {
-		 c.Failed(ctx, http.StatusInternalServerError, "", app_error.UnknownError(""))
-		 return
-	 }
- 
-	 c.Success(ctx, http.StatusOK, "", "Successfully retrieved all user data", res)
- }
- 
- func (c *UserController) GetOne(ctx *gin.Context) {
-	 id, err := strconv.Atoi(ctx.Param("id"))
-	 if err != nil {
-		 c.Failed(ctx, http.StatusBadRequest, "X01", app_error.InvalidError("invalid id"))
-		 return
-	 }
- 
-	 res, err := c.usecase.GetOne(id)
-	 if err != nil {
-		 c.Failed(ctx, http.StatusNotFound, "X04", app_error.DataNotFoundError(fmt.Sprintf("user with id %d not found", id)))
-		 return
-	 }
- 
-	 c.Success(ctx, http.StatusOK, "", fmt.Sprintf("Successfully retrieved user with ID %d", id), res)
- }
- 
+type UserController struct {
+	BaseController
+	router  *gin.RouterGroup
+	usecase usecase.UserUsecase
+}
+
+func NewUserController(r *gin.RouterGroup, u usecase.UserUsecase) *UserController {
+	controller := UserController{
+		router:  r,
+		usecase: u,
+	}
+
+	// define routes
+	r.GET("/users", controller.GetAll)
+	r.GET("/users/:id", controller.GetOne)
+	r.POST("/users", controller.Add)
+	r.PUT("/users/:id", controller.Edit)
+	r.DELETE("/users/:id", controller.Remove)
+
+	return &controller
+}
+
+func (c *UserController) GetAll(ctx *gin.Context) {
+	res, err := c.usecase.GetAll()
+	if err != nil {
+		c.Failed(ctx, http.StatusInternalServerError, "", app_error.UnknownError(""))
+		return
+	}
+
+	c.Success(ctx, http.StatusOK, "", "Successfully retrieved all user data", res)
+}
+
+func (c *UserController) GetOne(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		c.Failed(ctx, http.StatusBadRequest, "X01", app_error.InvalidError("invalid id"))
+		return
+	}
+
+	res, err := c.usecase.GetOne(id)
+	if err != nil {
+		c.Failed(ctx, http.StatusNotFound, "X04", app_error.DataNotFoundError(fmt.Sprintf("user with id %d not found", id)))
+		return
+	}
+
+	c.Success(ctx, http.StatusOK, "", fmt.Sprintf("Successfully retrieved user with ID %d", id), res)
+}
+
 func (c *UserController) Add(ctx *gin.Context) {
 	var user entity.User
 
@@ -87,24 +87,24 @@ func (c *UserController) Add(ctx *gin.Context) {
 	c.Success(ctx, http.StatusCreated, "01", "Successfully created new user", res)
 }
 
+func (c *UserController) Edit(ctx *gin.Context) {
+	var user entity.User
+
+	if err := ctx.BindJSON(&user); err != nil {
+		c.Failed(ctx, http.StatusBadRequest, "", app_error.InvalidError("invalid request body"))
+		return
+	}
  
- func (c *UserController) Edit(ctx *gin.Context) {
-	 var user entity.User
-	 if err := ctx.BindJSON(&user); err != nil {
-		 c.Failed(ctx, http.StatusBadRequest, "X01", app_error.InvalidError("invalid request body"))
-		 return
-	 }
- 
-	 res, err := c.usecase.Edit(&user)
-	 if err != nil {
-		 c.Failed(ctx, http.StatusNotFound, "X04", app_error.DataNotFoundError(fmt.Sprintf("user with id %d not found", user.ID)))
-		 return
-	 }
- 
-	 c.Success(ctx, http.StatusOK, "", fmt.Sprintf("Successfully updated user with ID %d", user.ID), res)
- }
- 
- func (c *UserController) Remove(ctx *gin.Context) {
+	res, err := c.usecase.Edit(&user)
+	if err != nil {
+		c.Failed(ctx, http.StatusNotFound, "X04", app_error.DataNotFoundError(fmt.Sprintf("user with id %d not found", user.ID)))
+		return
+	}
+
+	c.Success(ctx, http.StatusOK, "", fmt.Sprintf("Successfully updated user with ID %d", user.ID), res)
+}
+
+func (c *UserController) Remove(ctx *gin.Context) {
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
 		c.Failed(ctx, http.StatusBadRequest, "X01", app_error.InvalidError("invalid id"))
